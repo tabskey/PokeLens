697630433cd5a4fdce9fbb67cb626493142cf526
Tabatha <tabmacedo@gmail.com>
2025-09-05 22:01:50 -0300

    Updated swagger :lipstick: :memo:

---
+++ PokeLens/Swagger/Filters/BooleanToGameVersionSchemaFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace PokeLens.Swagger.Filters;
+
+public class BooleanToGameVersionSchemaFilter : ISchemaFilter
+{
+	public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+	{
+		// Keep schema as boolean; just add helpful description for docs when applicable
+		if (schema.Type == "boolean" && context?.MemberInfo?.Name?.Equals("isHeartGold", StringComparison.OrdinalIgnoreCase) == true)
+		{
+			schema.Description = "Gen IV version selector: HeartGold or SoulSilver";
+		}
+	}
+}
